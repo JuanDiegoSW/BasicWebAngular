37b3f5a98b7980d2137baeec847922a7a1fb709a
JuanDiegoSW <juan.lazaro.m@tecsup.edu.pe>
2021-10-20 18:37:00 -0500

    mejorando parte2

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist/my-first-project'));
+/*app.use(express.static(__dirname+'/dist/my-first-project'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/my-first-project/src/index.html'));
 
-});
+});*/
+app.get(express.static('./dist/my-first-project'));
+app.get('/*', (req,res) =>
+    res.sendFile('index.html', {root:'dist/my-first-project/'}),
+
+);
 app.listen(process.env.PORT||8080);
